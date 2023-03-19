72e4d917d9e3aee0cc3c0521965919ebc3aa4013
marinaasv <122703469+marinaasv@users.noreply.github.com>
2023-03-19 10:48:22 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# sistemaventa
+# Sistema de venta PHP y Mysql
+![pos-portada](https://user-images.githubusercontent.com/88554898/209576200-a3da731a-7f2f-46fa-93e6-4a943949df1f.png)
---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+define('RUTA', 'http://localhost/sistemaventa/');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_NAME', 'sistemaventa');
+define('DB_PASS', '');
+?>
